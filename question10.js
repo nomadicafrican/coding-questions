608a6d155186b282a9431bfd3aa242471db05c3e
Ammar <ammar.kusow2@gmail.com>
2022-02-05 11:40:51 -0500

    question 10

--- question10.js
+++ question10.js
@@ -1,10 +1,15 @@
 var isPalindrome = function (x) {
-  const number = x.toString().split("").reverse().join("");
+  const number = parseInt(x.toString().split("").reverse().join(""));
   // to make a number reverse you would first need to make it a string,
   //split it at every letter and placing it in an array, then reversing said array
   // and joining at the end.
   // for a string you dont need to do toString
-  return number;
+  // ParseINt is to covert to integer
+  console.log("number", number);
+  if (x === number) {
+    return true;
+  }
+  return false;
 };
 
-console.log(isPalindrome("Ammar"));
+console.log(isPalindrome(-222));
